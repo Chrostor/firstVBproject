9dafe91576fe0a06abf0f60b128043464648493f
Chrostor <Chrostor@users.noreply.github.com>
2016-06-29 00:11:44 -0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Dim SQL As New SQLControl
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If SQL.HasConnection = True Then
+            ' MsgBox("Sucessfully Connected")
+            ' SQL.RunQuery("select first from employees")
+        End If
+    End Sub
+
+    Private Sub cmdQuery_Click(sender As Object, e As EventArgs) Handles cmdQuery.Click
+        If txtQuery.Text <> "" Then
+
+
+            If SQL.HasConnection = True Then
+                SQL.RunQuery(txtQuery.Text)
+
+                If SQL.SQLDataset.Tables.Count > 0 Then
+                    DGVData.DataSource = SQL.SQLDataset.Tables(0)
+                End If
+            End If
+        End If
+    End Sub
+End Class
---
+++ SQLControl.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+
+Public Class SQLControl
+    Public SQLCon As New SqlConnection With {.ConnectionString = "Server=(localdb)\MSSQLLocalDB;Database=EmployeeDatabase;"}
+    Public SQLCmd As SqlCommand
+    Public SQLDA As SqlDataAdapter
+    Public SQLDataset As DataSet
+
+    Public Function HasConnection() As Boolean
+        Try
+            SQLCon.Open()
+
+            SQLCon.Close()
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Sub RunQuery(Query As String)
+        Try
+            SQLCon.Open()
+            SQLCmd = New SqlCommand(Query, SQLCon)
+
+            'LOAD SQL RECORDS FOR DATAGRID
+            SQLDA = New SqlDataAdapter(SQLCmd)
+            SQLDataset = New DataSet
+            SQLDA.Fill(SQLDataset)
+
+            ' READ DIRECTLY FROM THE DATABASE
+            'Dim R As SqlDataReader = SQLCmd.ExecuteReader
+            'While R.Read
+            'MsgBox(R.GetName(0) & ": " & R(0))
+            'End While
+
+            SQLCon.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            If SQLCon.State = ConnectionState.Open Then SQLCon.Close()
+        End Try
+    End Sub
+
+End Class
